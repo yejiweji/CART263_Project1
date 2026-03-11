5bc924b7f7f29781b431d8cbe31be85185ad5f76
Yejin Oh <yejinnn.oh@gmail.com>
2026-03-11 18:13:23 -0400

    assets all in (minus chinese door)

--- js/script.js
+++ js/script.js
@@ -59,20 +59,3 @@ document.addEventListener("DOMContentLoaded", function () {
 function closeModal() {
     document.getElementById("confirmationModal").style.display = "none";
 }
-
-
-
-const doorClosed = document.getElementById('korean_door');
-const doorOpen = document.getElementById('korean_door_open');
-
-doorOpen.style.display = 'none'; // hide open door initially
-
-doorClosed.addEventListener('mouseenter', () => {
-  doorClosed.style.display = 'none';
-  doorOpen.style.display = 'block';
-});
-
-doorOpen.addEventListener('mouseleave', () => {
-  doorOpen.style.display = 'none';
-  doorClosed.style.display = 'block';
-});
--- museum/museum.js
+++ museum/museum.js
@@ -1,14 +1,15 @@
-const doorClosed = document.getElementById('korean_door');
-const doorOpen = document.getElementById('korean_door_open');
+// door hovering 
+// const doorClosed = document.getElementById('korean_door');
+// const doorOpen = document.getElementById('korean_door_open');
 
-doorOpen.style.display = 'none'; // hide open door initially
+// doorOpen.style.display = 'none'; // hide open door initially
 
-doorClosed.addEventListener('mouseenter', () => {
-  doorClosed.style.display = 'none';
-  doorOpen.style.display = 'block';
-});
+// doorClosed.addEventListener('mouseenter', () => {
+//   doorClosed.style.display = 'none';
+//   doorOpen.style.display = 'block';
+// });
 
-doorOpen.addEventListener('mouseleave', () => {
-  doorOpen.style.display = 'none';
-  doorClosed.style.display = 'block';
-});
+// doorOpen.addEventListener('mouseleave', () => {
+//   doorOpen.style.display = 'none';
+//   doorClosed.style.display = 'block';
+// });
