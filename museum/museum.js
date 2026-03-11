b29b99cc133bd3cb64462b5bc335b2e03e1766bf
Yejin Oh <yejinnn.oh@gmail.com>
2026-03-11 17:15:34 -0400

    added frames

---
+++ museum/museum.js
@@ -0,0 +1,14 @@
+const doorClosed = document.getElementById('korean_door');
+const doorOpen = document.getElementById('korean_door_open');
+
+doorOpen.style.display = 'none'; // hide open door initially
+
+doorClosed.addEventListener('mouseenter', () => {
+  doorClosed.style.display = 'none';
+  doorOpen.style.display = 'block';
+});
+
+doorOpen.addEventListener('mouseleave', () => {
+  doorOpen.style.display = 'none';
+  doorClosed.style.display = 'block';
+});
